ec2abdbd7ad36b42f8a5ce994f2dc22baae8ca52
sakib-taskeasy <sakib@taskeasy.io>
2023-08-03 23:26:34 +0600

    wip : migration

---
+++ database/migrations/2023_08_03_160829_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id');
+            $table->string('name')->nullable();
+            $table->date('estimated_completion_date');
+            $table->bigInteger('estimated_budget');
+            $table->bigInteger('total_budget')->nullable();
+            $table->longText('comment')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
