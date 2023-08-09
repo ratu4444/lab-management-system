4a81b9c5591affc51a85cd9dd5a694815011372e
sakib-taskeasy <sakib@taskeasy.io>
2023-08-09 21:28:47 +0600

    updated

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inspection;
+use App\Models\Payment;
 use App\Models\Project;
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -21,7 +24,61 @@ class DashboardController extends Controller
             ->withCount('projects')
             ->paginate(10, ['*'], 'client_page');
 
-        return view('index', compact('projects', 'clients'));
+        $total_projects = [
+            'type'              => 'projects',
+            'heading'           => 'Total Projects',
+            'card_icon'         => 'fa fa-list',
+            'card_background'   => 'l-bg-green',
+            'count'             => Project::count(),
+            'url'               => route('project.index')
+        ];
+
+        $total_clients = [
+            'type'              => 'clients',
+            'heading'           => 'Total Clients',
+            'card_icon'         => 'fa fa-users',
+            'card_background'   => 'l-bg-orange',
+            'count'             => User::where('is_client', true)->count(),
+            'url'               => route('client.index')
+        ];
+
+        $total_tasks = [
+            'type'              => 'tasks',
+            'heading'           => 'Total Tasks',
+            'card_icon'         => 'fa fa-clipboard-list',
+            'card_background'   => 'l-bg-red',
+            'count'             => Task::count(),
+            'url'               => null,
+        ];
+
+        $total_payments = [
+            'type'              => 'payments',
+            'heading'           => 'Total Payments',
+            'card_icon'         => 'fa fa-dollar-sign',
+            'card_background'   => 'l-bg-cyan',
+            'count'             => Payment::sum('amount'),
+            'url'               => null,
+        ];
+
+        $total_inspections = [
+            'type'              => 'inspections',
+            'heading'           => 'Total Inspections',
+            'card_icon'         => null,
+            'card_background'   => 'l-bg-purple-dark',
+            'count'             => Inspection::count(),
+            'url'               => null,
+        ];
+
+
+        $reports = [
+            $total_projects,
+            $total_clients,
+            $total_tasks,
+            $total_payments,
+            $total_inspections
+        ];
+
+        return view('index', compact('projects', 'clients', 'reports'));
     }
 
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,28 @@
 @section('content')
     <div class="container">
         <div class="row">
+            @foreach($reports as $report)
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card {{ $report['card_background'] ?? 'l-bg-cyan' }}">
+                        <div class="card-statistic-3">
+                            <div class="card-icon card-icon-large">
+                                <i class="{{ $report['card_icon'] ?? 'fa fa-globe' }}"></i>
+                            </div>
+                            <div class="card-content">
+                                <h4 class="card-title text-white-50" style="min-height: 60px">{{ $report['heading'] }}</h4>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <h3 class="card-subtitle m-0 text-light card-value">{{ $report['count'] }}</h3>
+                                    @if($report['url'])
+                                        <a href="{{ $report['url'] }}" class="btn btn-primary btn-sm shadow-lg" style="z-index: 999">Show</a>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div class="row">
             <div class="col-12">
                 <div class="card card-primary">
                     <div class="card-header d-flex justify-content-between">
@@ -33,4 +55,22 @@
 @endsection
 
 @push('js')
+    <script>
+        $(document).ready(function() {
+            var speed = 1000;
+
+            $('.card-value').each(function() {
+                var valueElement = $(this);
+                var value = valueElement.text();
+
+                valueElement.text(0); // set initial value to 0
+                $({ value: 0 }).animate({ value: value }, {
+                    duration: speed,
+                    step: function() {
+                        valueElement.text(Math.ceil(this.value));
+                    }
+                });
+            });
+        });
+    </script>
 @endpush
