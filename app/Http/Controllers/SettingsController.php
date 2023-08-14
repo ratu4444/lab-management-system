61f668aa05ee04c02de2f18f03dfdcd82ac044c3
ratu4444 <imran15-11178@diu.edu.bd>
2023-08-14 20:46:14 +0600

    Settings all c done

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SettingsInspection;
+use App\Models\SettingsPayment;
 use App\Models\SettingsTask;
 use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
-    public function create(){
+    public function taskCreate(){
         return view('settings.create-task');
     }
 
-    public function store(Request $request){
+    public function taskStore(Request $request){
 
         $task_data = [
             'name'   => $request->name,
@@ -20,8 +22,43 @@ class SettingsController extends Controller
             'comment'=> $request->comment,
         ];
 
-        $task_settings = SettingsTask::create($task_data);
+        $settings_task = SettingsTask::create($task_data);
         return redirect()->back();
-
     }
+
+    public function paymentCreate(){
+        return view('settings.create-payment');
+    }
+
+    public function paymentStore(Request $request){
+
+        $payment_data = [
+            'name'   => $request->name,
+            'amount' => $request->amount,
+            'payment_method' => $request->payment_method,
+            'comment'=> $request->comment,
+        ];
+
+
+        $settings_payment = SettingsPayment::create($payment_data);
+        return redirect()->back();
+    }
+
+    public function inspectionCreate(){
+        return view('settings.create-inspection');
+    }
+
+    public function inspectionStore(Request $request){
+
+        $inspection_data = [
+            'name'   => $request->name,
+            'status' => $request->status,
+            'comment'=> $request->comment,
+        ];
+
+
+        $settings_inspection = SettingsInspection::create($inspection_data);
+        return redirect()->back();
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -50,12 +50,19 @@ Route::middleware('auth')->group(function () {
             Route::post('{project_id}/inspection', [InspectionController::class, 'store'])->name('inspection.store');
             Route::get('inspection/{inspection_id}/edit', [InspectionController::class, 'edit'])->name('inspection.edit');
             Route::post('inspection/{inspection_id}/update', [InspectionController::class, 'update'])->name('inspection.update');
-//      SETTINGS
-            Route::get('settings/create', [SettingsController::class, 'create'])->name('settings.create');
-            Route::post('settings/store', [SettingsController::class, 'store'])->name('settings.store');
-
-
         });
+
+//        SETTINGS
+//            SETTINGS TASK
+            Route::get('settings/task/create', [SettingsController::class, 'taskCreate'])->name('settings.task.create');
+            Route::post('settings/task/store', [SettingsController::class, 'taskStore'])->name('settings.task.store');
+//            SETTINGS PAYMENTS
+            Route::get('settings/payment/create', [SettingsController::class, 'paymentCreate'])->name('settings.payment.create');
+            Route::post('settings/payment/store', [SettingsController::class, 'paymentStore'])->name('settings.payment.store');
+//            SETTINGS INSPECTION
+            Route::get('settings/inspection/create', [SettingsController::class, 'inspectionCreate'])->name('settings.inspection.create');
+            Route::post('settings/inspection/store', [SettingsController::class, 'inspectionStore'])->name('settings.inspection.store');
+
 //  CLIENT
         Route::prefix('client')->group(function () {
             Route::get('/', [ClientController::class, 'index'])->name('client.index');
